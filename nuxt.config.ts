bf97c1d0728c9a6f0baa198f5c2904f7114a1aa4
MR.LEE <leesunggi92@gmail.com>
2025-12-13 06:50:16 +0900

    ssr: true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  ssr: false,  // 추가
+  ssr: true,  // 추가
   app: {
     baseURL: BASE,   // ★ 저장소 이름
     buildAssetsDir: '/_nuxt/', // 기본값이지만 명시해도 됨
