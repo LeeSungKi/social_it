1fb6197a7da95d283fbc23b1faed4cfdd88d1c58
MR.LEE <leesunggi92@gmail.com>
2025-12-13 06:42:56 +0900

    파비콘 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ const SITE_URL = process.env.NUXT_PUBLIC_SITE_URL || 'https://leesungki.github.i
 const BASE = '/social_it/'
 const FULL_URL = `${SITE_URL}${BASE}`
 const OG_IMAGE = `${FULL_URL}images/logo.png`
+const FAVICON = `${FULL_URL}favicon.ico`
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -24,11 +25,22 @@ export default defineNuxtConfig({
         { property: 'og:type', content: 'website' },
         { property: 'og:url', content: FULL_URL },
         { property: 'og:image', content: OG_IMAGE },
+        { property: 'og:image:secure_url', content: OG_IMAGE },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        { property: 'og:image:alt', content: 'Social It 로고' },
+        { property: 'og:site_name', content: 'Social It' },
+        { property: 'og:locale', content: 'ko_KR' },
         // Twitter
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:title', content: 'Social It - Social Matching Party' },
         { name: 'twitter:description', content: "Social It - 감각적인 소셜 매칭 파티. 일정 확인과 신청, 위치 안내 및 소식 구독" },
         { name: 'twitter:image', content: OG_IMAGE },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: FAVICON },
+        { rel: 'shortcut icon', type: 'image/x-icon', href: FAVICON },
+        { rel: 'apple-touch-icon', href: OG_IMAGE }
       ],
     },
   },
